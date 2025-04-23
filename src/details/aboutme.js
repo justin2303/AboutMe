6c77cba1207de9e6dfb5ccb840e7de3d314b594d
Your Name <justin.ypc@gmail.com>
2025-04-22 18:16:56 -0700

    finished skills page

--- src/details/aboutme.js
+++ src/details/aboutme.js
@@ -12,6 +12,14 @@ const AboutMe = () => {
         setSkills(!showSkills);
         setAbout(!showAbout);
       };
+    const HandleDownload = () => {
+        const link = document.createElement('a');
+        link.href = `${process.env.PUBLIC_URL}/Justin_Resume.pdf`; // Replace with your actual file name
+        link.download = 'Justin_Resume.pdf'; 
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
     return (
         <div>
             <div className='navbar-wrapper'>
@@ -42,13 +50,20 @@ const AboutMe = () => {
                     {showSkills && (<div >
                         <div className='Qcard'> 
                             <p className='introText'>
-                           
+                           <b>Here's a Brief Overview of my skills:</b><br /> 
+                           <b>Languages:</b>&nbsp;&nbsp;&nbsp;&nbsp;Go, JavaScript, Python, MySQL, C++, HTML/CSS, Bash, Groovy, Java, C, R <br />
+                           <b>Frameworks:</b>&nbsp;&nbsp;&nbsp;&nbsp;React.js, Pytest, Node.js, Angular, Django, Flask, Celery <br />
+                           <b>Tools:</b>&nbsp;&nbsp;&nbsp;&nbsp;Docker, AWS, Github, bash, pdb, Jupyter Notebooks, VS Code, g++ <br />
+                           <b>Concepts:</b>&nbsp;&nbsp;&nbsp;&nbsp;Algorithms and data structures, Multithreading, RESTful API Design, SQL/no-SQL
+                           DBs, Event-Driven, Architecture, Version Control, Agile/Scrum <br />
                         </p>
                         </div>
+                        <br />
                         <div className='Qcard'> 
                             <p className='introText'>
-
+                            Here's a version of my resume to download if you want:
                             </p>
+                            <img src='/download.webp' className='downloadButton' onClick={HandleDownload}></img>
                         </div>
                         </div>
                     )}
